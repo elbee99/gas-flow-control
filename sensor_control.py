609ece056bcead404b95017699f5eeeb3959e53c
nanoint-labPC <87809184+elbee99@users.noreply.github.com>
2023-03-24 11:17:05 +0000

    Started plotting (incomplete)
    fABCY WORDS

--- sensor_control.py
+++ sensor_control.py
@@ -1,5 +1,6 @@
 import serial
-
+import time
+import matplotlib.pyplot as plt
 serial_output = ""
 
 # opens the COM3 port which is what the O2 sensor was when I plugged it in
@@ -10,7 +11,11 @@ for port in range(1,17):
     try:
         with serial.Serial('COM{}'.format(port), 57600, timeout=1) as ser: 
             print(ser.name)
-
+            start_time = time.time()
+            oxygen_ppm_list = []
+            time_list = []
+            plt.figure()
+            # plt.show()
             while True: 
                 
                 ser.write('D'.encode('ascii'))
@@ -23,10 +28,15 @@ for port in range(1,17):
                             serial_output_ascii = serial_output.decode('ascii')
                         except Exception: 
                             print('Data communication was not successful)')
+                            pass
 
                         oxygen_ppm = serial_output_ascii.split(',')[0]
                         oxygen_ppm = oxygen_ppm.replace('d','')
+                        # print(serial_output_ascii)
                         print(oxygen_ppm)
+                        oxygen_ppm_list.append(float(oxygen_ppm))
+                        time_list.append(time.time()-start_time)
+                        plt.plot(time_list,oxygen_ppm_list)
     except serial.serialutil.SerialException:
         pass
             
