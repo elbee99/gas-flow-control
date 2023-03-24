8a1e55149207839c57e5b0c4e7db5aa42b75be17
nanoint-labPC <87809184+elbee99@users.noreply.github.com>
2023-03-24 10:57:27 +0000

    Update sensor_control.py
    Added in ability to scan COM ports until right one is found, added in exceptions to except statements to avoid breaking

--- sensor_control.py
+++ sensor_control.py
@@ -5,20 +5,28 @@ serial_output = ""
 # opens the COM3 port which is what the O2 sensor was when I plugged it in
 # check to see if it is COM3 before running
 # Will try optimise so it selects automatically soon
-with serial.Serial('COM3', 576000, timeout=1) as ser:
-    print(ser.name)
 
-    while True:
-        # according to manual send D to get response
-        ser.write('D'.encode('ASCII')) 
-        if(ser.in_waiting > 0): #checks if there is bytes to be read
-            serial_output = ser.readline() #reads to newline character
-            try: 
-                serial_output_ascii = serial_output.decode('ascii')
-            except: 
-                print('Data communication was not successful)')
+for port in range(1,17):
+    try:
+        with serial.Serial('COM{}'.format(port), 57600, timeout=1) as ser: 
+            print(ser.name)
 
-            oxygen_ppm = serial_output_ascii.split(',')[0]
-            oxygen_ppm = oxygen_ppm.replace('d','')
-            print(oxygen_ppm)
+            while True: 
+                
+                ser.write('D'.encode('ascii'))
+                
+                if(ser.in_waiting > 0):
+                        serial_output = ser.readline() 
+                        #print(serial_output)
+                        
+                        try: 
+                            serial_output_ascii = serial_output.decode('ascii')
+                        except Exception: 
+                            print('Data communication was not successful)')
+
+                        oxygen_ppm = serial_output_ascii.split(',')[0]
+                        oxygen_ppm = oxygen_ppm.replace('d','')
+                        print(oxygen_ppm)
+    except serial.serialutil.SerialException:
+        pass
             
