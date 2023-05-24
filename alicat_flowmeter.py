5efc83656f464463ef65cafb5fab923322c29314
nanoint-labPC <87809184+elbee99@users.noreply.github.com>
2023-05-24 14:56:49 +0100

    Update alicat_flowmeter.py

--- alicat_flowmeter.py
+++ alicat_flowmeter.py
@@ -9,5 +9,5 @@ print(flow_controller_Ar.get())
 flow_controller_O2.set_gas('O2')
 flow_controller_Ar.set_gas('Ar')
 
-flow_controller_O2.set_flow_rate(5)
-flow_controller_Ar.set_flow_rate(95)
+flow_controller_O2.set_flow_rate(0)
+flow_controller_Ar.set_flow_rate(0)
